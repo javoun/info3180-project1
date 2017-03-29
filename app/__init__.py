fb758bbddedebeea2d6ac74b46a1f11af09756bd
javoun <javounlong@yahoo.com>
2017-03-29 11:13:28 +0000

    Final commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "this is a super secure key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password@localhost/project1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+# Location where image uploads will be stored
+UPLOAD_FOLDER = './app/static/uploads'
+
+#IMAGES = set(['png', 'jpg', 'jpeg', 'gif'])
+
 
 db = SQLAlchemy(app)
 
-# Flask-Login login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
 app.config.from_object(__name__)
-from app import views
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -5,11 +5,15 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
-from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash, json, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
-from forms import LoginForm
-from models import UserProfile
+from forms import ProfileForm
+from app.models import UserProfile
+from werkzeug.utils import secure_filename
+import time
+import os
+import random
 
 ###
 # Routing for your application.
@@ -17,88 +21,145 @@ from models import UserProfile
 
 @app.route('/')
 def home():
-    """Render website's home page."""
+    """ Render website's home page."""
     return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
-
-@app.route('/secure-page/')
-@login_required
-def secure_page():
-    """Render a secure page on our website that only logged in users can access."""
-    return render_template('secure_page.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        # if user is already logged in, just redirec them to our secure page
-        # or some other page like a dashboard
-        return redirect(url_for('secure_page'))
-
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
-    form = LoginForm()
-    # Login and validate the user.
-    if request.method == 'POST' and form.validate_on_submit():
-        # Query our database to see if the username and password entered
-        # match a user that is in the database.
-        username = form.username.data
-        password = form.password.data
-
-        user = UserProfile.query.filter_by(username=username, password=password)\
-        .first()
-
-        if user is not None:
-            # If the user is not blank, meaning if a user was actually found,
-            # then login the user and create the user session.
-            # user should be an instance of your `User` class
-            login_user(user)
-
-            flash('Logged in successfully.', 'success')
-            next = request.args.get('next')
-            return redirect(url_for('secure_page'))
-        else:
-            flash('Username or Password is incorrect.', 'danger')
-
+@app.route('/profile/', methods=['GET', 'POST'])
+def add_profile():
+    """ Render a page for adding a user profile """
+    
+    # Generate form
+    form = ProfileForm()
+    # Check request type
+    if request.method == "POST":
+        # Validate form
+        if form.validate_on_submit():
+            # Get form values
+            first_name = request.form['firstname']
+            last_name = request.form['lastname']
+            username = request.form['username']
+            age = request.form['age']
+            biography = request.form['biography']
+            gender = request.form['gender']
+            # Uploads folder
+            imageFolder = app.config["UPLOAD_FOLDER"]
+            # Get picture file
+            imageFile = request.files['image']
+            # Check if empty
+            if imageFile.filename == '':
+                # Store default profile pic in DB
+                imageName = "profile-default.gif"
+            else:
+                # Secure file
+                imageName = secure_filename(imageFile.filename)
+                # Save to uploads directory
+                imageFile.save(os.path.join(imageFolder, imageName))
+            # Loop to find a unique id
+            while True:
+                # Generate a random userid
+                userid = random.randint(620000000, 620099999)
+                # Search for this userid
+                result = UserProfile.query.filter_by(userid=userid).first() 
+                # Check if not found
+                if result is None:
+                    # Unique; Exit loop
+                    break
+            # Generate the date the user was created on
+            created_on = timeinfo()
+            # Store data in database
+            profile = UserProfile(userid,first_name,last_name,username,age,gender,biography,imageName,created_on)
+            db.session.add(profile)
+            db.session.commit()
+            # Flash success message
+            flash('New user profile sucessfully added', 'success')
+            # Redirect to list of profiles
+            return redirect(url_for('list_profiles'))
+        
+    # Display any errors in form
     flash_errors(form)
-    return render_template('login.html', form=form)
+            
+    return render_template('add_profile.html', form=form)
 
-@app.route("/logout")
-@login_required
-def logout():
-    # Logout the user and end the session
-    logout_user()
-    flash('You have been logged out.', 'danger')
-    return redirect(url_for('home'))
-
-@login_manager.user_loader
-def load_user(id):
-    return UserProfile.query.get(int(id))
-
-# Flash errors from the form if validation fails
-def flash_errors(form):
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-            ), 'danger')
-
+@app.route('/profiles', methods=['GET','POST'])
+def list_profiles():
+    """ Render a page for viewing a list of all user profiles """
+    
+    # Get all user profiles from database
+    profiles = db.session.query(UserProfile).all()
+    
+    if request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
+        # Initialize a profile list
+        profileList = []
+        # Get each profile
+        for profile in profiles:
+            # Create dictionary format
+            profileDict = {'username': profile.username, 'userid': profile.userid}
+            # Add to list of profiles
+            profileList.append(profileDict)
+        # Convert list to JSON data
+        #jsonProfiles = json.dumps(profileList)
+        # Generate JSON output
+        return jsonify(users=profileList)
+    else:
+        if not profiles:
+            # Display message
+            flash('No users have been added. Add a user below.', 'danger')
+            # Redirect to add user page
+            return redirect(url_for('add_profile'))
+        # Render page for viewing all user profiles
+        return render_template('profiles_listing.html', profiles=profiles)
+    
+@app.route('/profile/<userid>', methods=['GET','POST'])
+def view_profile(userid):
+    """ Render an individual user profile page """
+    
+    # Search for given userid
+    user_profile = UserProfile.query.filter_by(userid=userid).first()
+    # Check if found
+    if user_profile is not None:
+        # Check request type
+        if request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
+            # Generate JSON output
+            return jsonify(userid=user_profile.userid, username=user_profile.username, image=user_profile.image, gender=user_profile.gender, age=user_profile.age, profile_created_on=user_profile.created_on )
+        else:
+            # Render user's profile page
+            return render_template('view_profile.html', user_profile=user_profile)
+    else: # Not found
+        # Check request type
+        if request.method == 'GET':
+            
+            # Flash error message
+            flash(' User not found.')
+            
+            # Redirect to list of users
+            return redirect(url_for('list_profiles'))
+           
+        elif  request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
+            # Return empty set (null)
+            return jsonify(user_profile)
+    
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+def timeinfo():
+    """ Returns the current datetime """
+    return time.strftime("%d %b %Y")
+
+def flash_errors(form):
+    """Flashes form errors"""
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                 error ), 'danger')
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
-
 
 @app.after_request
 def add_header(response):
